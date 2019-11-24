962fa714f1547de6cfadbdcfa93b98160eab09de
Adrien <adrien.arra@live.fr>
2019-11-24 18:53:43 +0100

    add support for weapon card

--- src/main/model/Board.java
+++ src/main/model/Board.java
@@ -2,11 +2,14 @@ package main.model;
 
 import main.card.Card;
 import main.card.Minion;
+import main.card.Weapon;
 
 public class Board {
     private CardContainer<Card> deck;
     private CardContainer<Minion> graveyard;
     private CardContainer<Minion> minions;
+
+    private Weapon weapon;
 
     public Board(CardContainer<Card> deck) {
         this.deck = deck;
@@ -17,6 +20,10 @@ public class Board {
 
     public void summonMinion(Minion minion) {
         minions.add(minion);
+    }
+
+    public void summonWeapon(Weapon weapon) {
+        this.weapon = weapon;
     }
 
     public void placeInGraveyard(Minion minion) {
