50b989446a1076f40df8e6287e44dd716edb7015
petergiteube <peterduvauchelle97@gmail.com>
2019-11-25 22:49:49 +0100

    adding labels to cardview

--- src/main/ui/CardView.java
+++ src/main/ui/CardView.java
@@ -4,12 +4,9 @@ import main.card.Card;
 import main.card.Minion;
 import main.events.ICardListener;
 
-import javax.imageio.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+
 
 public class CardView extends JPanel implements ICardListener {
 
@@ -46,73 +43,68 @@ public class CardView extends JPanel implements ICardListener {
     }
 
     public static void main(String[] args) {
-        Minion minion = new Minion("test",2,1,1);
-        CardView view = new CardView(minion);
         JFrame gameInstance = new JFrame();
         gameInstance.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Minion minion = new Minion("test",2,1,1);
+        CardView view = new CardView(minion);
         gameInstance.add(view);
-        gameInstance.pack();
-        gameInstance.setSize(1500,1500);
+        gameInstance.setSize(143,229);
         gameInstance.setVisible(true);
+       // gameInstance.pack();
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        BufferedImage backGround = null;
-        try {
-            backGround = ImageIO.read(new File("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\frame-minion-neutral.png"));
-        } catch (IOException e) {
-            System.out.println("error");
-            e.printStackTrace();
-        }
-        g.drawImage(backGround,0,0,null);
+        ImageIcon backGround = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\frame-minion.png");
+        g.drawImage(backGround.getImage(),0,0,null);
     }
+
+
 
     public void initComponent() {
 
-        ImageIcon manaIcon = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\cost-mana.png");
-        ImageIcon attackIcon = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\attack-minion.png");
+        ImageIcon manaIcon = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\mana-cost.png");
+        ImageIcon attackIcon = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\attack.png");
         ImageIcon healthIcon = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\health.png");
-        ImageIcon frameMinionIcon = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\frame-minion-neutral.png");
-        ImageIcon cardNameIcon  = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\name-banner-minion.png");
+        ImageIcon cardNameIcon  = new ImageIcon("C:\\Users\\Peter Duvauchelle\\Desktop\\SeiyantArena\\src\\ressources\\name-card.png");
 
 
-        GridLayout layout = new GridLayout(4,4);
+
+
+        GridLayout layout = new GridLayout(3,2);
+
         this.setLayout(layout);
 
-        this.JcardNameLabel = new JLabel();
-        this.JmanaLabel = new JLabel();
 
         this.JmanaLabel = new JLabel();
         this.JmanaLabel.setIcon(manaIcon);
         this.JmanaLabel.setHorizontalTextPosition(JLabel.CENTER);
         this.JmanaLabel.setVerticalTextPosition(JLabel.CENTER);
-        this.JmanaLabel.setLocation(0,0);
-
-        this.JattackLabel = new JLabel();
-        this.JattackLabel.setIcon(attackIcon);
-        this.JattackLabel.setHorizontalTextPosition(JLabel.CENTER);
-        this.JattackLabel.setVerticalTextPosition(JLabel.CENTER);
-        this.JattackLabel.setLocation(0,0);
-
-        this.JhealthLabel = new JLabel();
-        this.JhealthLabel.setIcon(healthIcon);
-        this.JhealthLabel.setHorizontalTextPosition(JLabel.CENTER);
-        this.JhealthLabel.setVerticalTextPosition(JLabel.CENTER);
-        this.JhealthLabel.setLocation(0,0);
 
         this.JcardNameLabel = new JLabel();
         this.JcardNameLabel.setIcon(cardNameIcon);
         this.JcardNameLabel.setHorizontalTextPosition(JLabel.CENTER);
         this.JcardNameLabel.setVerticalTextPosition(JLabel.CENTER);
-        this.JcardNameLabel.setLocation(0,0);
+
+        this.JattackLabel = new JLabel();
+        this.JattackLabel.setIcon(attackIcon);
+        this.JattackLabel.setHorizontalTextPosition(JLabel.CENTER);
+        this.JattackLabel.setVerticalTextPosition(JLabel.CENTER);
+        this.JattackLabel.setHorizontalAlignment(JLabel.LEFT);
+
+        this.JhealthLabel = new JLabel();
+        this.JhealthLabel.setIcon(healthIcon);
+        this.JhealthLabel.setHorizontalTextPosition(JLabel.CENTER);
+        this.JhealthLabel.setVerticalTextPosition(JLabel.CENTER);
+        this.JhealthLabel.setHorizontalAlignment(JLabel.RIGHT);
 
 
-        this.add(this.JmanaLabel);
-        this.add(this.JattackLabel);
-        this.add(this.JhealthLabel);
-        this.add(this.JcardNameLabel);
+
+        add(this.JmanaLabel);
+        add(this.JcardNameLabel);
+        add(this.JattackLabel);
+        add(this.JhealthLabel);
+        this.setSize(200,200);
     }
 
     @Override
